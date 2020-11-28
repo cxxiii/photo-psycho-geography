8e596490c73981d7ccbe786797b281df1413a3e1
Nikolay Rademacher <nikolay@nikolayrademacher.net>
2020-11-28 08:10:51 +0100

    change to jsx and use yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,19 +3,5 @@ module.exports = {
     title: 'Katja Schmelzer',
     description: "A website showcasing Katja's photograhpy",
   },
-  plugins: [
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
-      options: {
-        production: true,
-        disable: !process.env.ANALYZE_BUNDLE_SIZE,
-        generateStatsFile: true,
-        analyzerMode: 'static',
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 }
