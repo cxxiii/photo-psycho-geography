3814a54aedab00beb9f3b7ea62f55aecea125a17
Nikolay Rademacher <nikolay@nikolayrademacher.net>
2020-11-30 06:03:53 +0100

    switch to next

--- src/components/ImgSlideShow.tsx
+++ src/components/ImgSlideShow.tsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react';
 // import styled from '@emotion/styled'
-import GALLERIES from '../galleries'
-import Layout from './Layout'
+import GALLERIES from '../galleries';
+import Layout from './Layout';
 
 const getRandomImg = () => {
-  const imgs = []
-  const randomGallery = Math.floor(Math.random() * 5)
+  const imgs = [];
+  const randomGallery = Math.floor(Math.random() * 5);
   const randomImgNum = Math.floor(
     Math.random() * GALLERIES[randomGallery].imgs.length,
-  )
-  imgs.push(GALLERIES[randomGallery].imgs[randomImgNum])
-  return imgs
-}
+  );
+  imgs.push(GALLERIES[randomGallery].imgs[randomImgNum]);
+  return imgs;
+};
 
 const ImgSlideShow = ({
   imgs = getRandomImg(),
@@ -19,42 +19,42 @@ const ImgSlideShow = ({
   showNavArrows,
   showCaptions,
 }) => {
-  const [image, setImage] = useState(imgs[0])
+  const [image, setImage] = useState(imgs[0]);
 
-  let currentImgNum = imgs.indexOf(image)
+  let currentImgNum = imgs.indexOf(image);
 
   const displayNextImg = useCallback(() => {
-    if (currentImgNum + 1 === imgs.length) return
-    setImage(imgs[++currentImgNum])
-    resetCaptionTimer()
-  }, [currentImgNum, imgs])
+    if (currentImgNum + 1 === imgs.length) return;
+    setImage(imgs[++currentImgNum]);
+    resetCaptionTimer();
+  }, [currentImgNum, imgs]);
 
   const displayPrevImg = useCallback(() => {
-    if (currentImgNum - 1 === -1) return
-    setImage(imgs[--currentImgNum])
-  }, [currentImgNum, imgs])
+    if (currentImgNum - 1 === -1) return;
+    setImage(imgs[--currentImgNum]);
+  }, [currentImgNum, imgs]);
 
   useEffect(() => {
     const navByArrowKeys = e => {
       if (e.code === 'ArrowRight') {
-        displayNextImg()
+        displayNextImg();
       } else if (e.code === 'ArrowLeft') {
-        displayPrevImg()
-      } else return
-    }
-    window.addEventListener('keydown', navByArrowKeys)
+        displayPrevImg();
+      } else return;
+    };
+    window.addEventListener('keydown', navByArrowKeys);
 
     return () => {
-      window.removeEventListener('keydown', navByArrowKeys)
-    }
-  }, [displayNextImg, displayPrevImg])
+      window.removeEventListener('keydown', navByArrowKeys);
+    };
+  }, [displayNextImg, displayPrevImg]);
 
   const resetCaptionTimer = () => {
-    document.querySelector('aside').hidden = true
+    document.querySelector('aside').hidden = true;
     setTimeout(() => {
-      document.querySelector('aside').hidden = false
-    }, 1000)
-  }
+      document.querySelector('aside').hidden = false;
+    }, 1000);
+  };
 
   return (
     <Layout>
@@ -64,7 +64,7 @@ const ImgSlideShow = ({
             <div
               id="arrow-left"
               role="button"
-              tabIndex="0"
+              tabIndex={0}
               className={`nav-arrow ${
                 currentImgNum === 0 ? 'nav-btn-inactive' : 'nav-btn-active'
               }`}
@@ -78,7 +78,7 @@ const ImgSlideShow = ({
             <div
               id="arrow-right"
               role="button"
-              tabIndex="0"
+              tabIndex={0}
               className={`nav-arrow ${
                 currentImgNum + 1 === imgs.length
                   ? 'nav-btn-inactive'
@@ -114,7 +114,7 @@ const ImgSlideShow = ({
         )}
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default ImgSlideShow
+export default ImgSlideShow;
